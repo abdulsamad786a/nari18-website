5531f22b9b7d542faefb03c13cb6eb6b06905ef5
abdulsamad786a <abd.sm.7783@gmail.com>
2026-01-18 12:56:51 +0530

    include file chnages

--- includes/config.php
+++ includes/config.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_SERVER','localhost');
-define('DB_USER','root');
-define('DB_PASS' ,'');
+define('DB_USER','u746862318_nari18');
+define('DB_PASS' ,'U746862318_nari18');
 define('DB_NAME', 'u746862318_nari18');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
@@ -9,8 +9,4 @@ if (mysqli_connect_errno())
 {
  echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
-
-// 2factor.in API Configuration
-define('TWO_FACTOR_API_KEY', '37380a45-d1bd-11f0-a6b2-0200cd936042');
-define('TWO_FACTOR_TEMPLATE', 'LoginOTP');
 ?>
